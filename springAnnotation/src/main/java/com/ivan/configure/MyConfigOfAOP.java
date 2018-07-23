ec4a04ddf508d10ea805494930c39c69528ae8cf
Ivan Lu <32642894+IvanLu1024@users.noreply.github.com>
2018-07-23 13:43:36 +0800

    2018.7.23——上午
    复习AOP部分
    
    使用打断点的方式：
          创建和注册AnnotationAwareAspectJAutoProxyCreator的过程

--- springAnnotation/src/main/java/com/ivan/configure/MyConfigOfAOP.java
+++ springAnnotation/src/main/java/com/ivan/configure/MyConfigOfAOP.java
@@ -38,14 +38,24 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  *              利用AspectJAutoProxyRegistrar自定义给容器中注册bean;
  *              internalAutoProxyCreator=AnnotationAwareAspectJAutoProxyCreator
  *
- *      给容器中注册一个AnnotationAwareAspectJAutoProxyCreator;
+ *      作用：给容器中注册一个AnnotationAwareAspectJAutoProxyCreator（注解驱动的切面自动代理创建器）;
+ *
  *   2.AnnotationAwareAspectJAutoProxyCreator：
  *      AnnotationAwareAspectJAutoProxyCreator
  *          ->AspectJAwareAdvisorAutoProxyCreator
  *              ->AbstractAdvisorAutoProxyCreator
- *                      implements SmartInstantiationAwareBeanPostProcessor(后置处理器),
- *                                  BeanFactoryAware
+ *                      implements SmartInstantiationAwareBeanPostProcessor(Bean的后置处理器),
+ *                                  BeanFactoryAware（）
+ *
  *                 关注后置处理器（bean初始化完成前后做的事情）、自动装配BeanFactoryAware
+ *
+ * 可以打上断点的方法：
+ *      AbstractAutoProxyCreator.setBeanFactory()
+ *      AbstractAutoProxyCreator.有后置处理器的逻辑；
+ *
+ *          AbstractAdvisorAutoProxyCreator.setBeanFactory()-》initBeanFactory()
+ *
+ *              AnnotationAwareAspectJAutoProxyCreator.initBeanFactory()
  *
  *  流程：
  *  *      1.传入配置类，创建ioc容器
@@ -147,9 +157,29 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  *  * 			2)、链式获取每一个拦截器，拦截器执行invoke方法，每一个拦截器等待下一个拦截器执行完成返回以后再来执行；
  *  * 				拦截器链的机制，保证通知方法与目标方法的执行顺序；
  *
- *
+ *      总结：
+ *          1.@EnableAspectJAutoProxy 开启AOP功能
+ *          2.@EnableAspectJAutoProxy 会给容器中注册一个组件AnnotationAwareAspectJAutoProxyCreator
+ *          3.AnnotationAwareAspectJAutoProxyCreator是一个后置处理器；
+ *          4.容器的创建流程：
+ *              1）、registerBeanPostProcessors（）注册后置处理器；创建AnnotationAwareAspectJAutoProxyCreator对象
+ *  * 			2）、finishBeanFactoryInitialization（）初始化剩下的单实例bean
+ *  * 				1）、创建业务逻辑组件和切面组件
+ *  * 				2）、AnnotationAwareAspectJAutoProxyCreator拦截组件的创建过程
+ *  * 				3）、组件创建完之后，判断组件是否需要增强（包装）
+ *  * 					是：切面的通知方法，包装成增强器（Advisor）;给业务逻辑组件创建一个代理对象（cglib）；
+ *  * 		5.执行目标方法：
+ *  * 			1）、代理对象执行目标方法
+ *  * 			2）、CglibAopProxy.intercept()；进行拦截
+ *                  拦截过程：
+ *  * 				1）、得到目标方法的拦截器链（增强器包装成拦截器MethodInterceptor）
+ *  * 				2）、利用拦截器的链式机制，依次进入每一个拦截器进行执行；
+ *  * 				3）、效果：
+ *  * 					正常执行：前置通知-》目标方法-》后置通知-》返回通知
+ *  * 					出现异常：前置通知-》目标方法-》后置通知-》异常通知
  *
  */
+//开启基于注解的aop模式
 @EnableAspectJAutoProxy
 @Configuration
 public class MyConfigOfAOP {
